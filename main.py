f00cb5926f44470550decbf4705861ae706c8361
hunter-er <sundownwhunter@gmail.com>
2025-08-03 16:48:03 -0500

    updated constants.py, added player object with rotation

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from constants import *
+from circleshape import *
+from player import *
 
 def main():
     print("Starting Asteroids!")
@@ -10,15 +12,24 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     run_game = True
 
+    clock = pygame.time.Clock()
+    dt = 0
+    player_x_init = SCREEN_WIDTH // 2
+    player_y_init = SCREEN_HEIGHT // 2
+    player = Player(player_x_init, player_y_init, PLAYER_RADIUS)
+
     while run_game:
         screen.fill("black")
+        player.update(dt)
+        player.draw(screen, "white", player.triangle(), 2)
+
         pygame.display.flip()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
---
+++ player.py
@@ -0,0 +1,34 @@
+import pygame
+from circleshape import CircleShape
+from constants import *
+
+class Player(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+        self.rotation = 0
+
+    # in the player class
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+    
+    def draw(self, screen, color, points, line_width):
+        # sub-classes must override
+        pygame.draw.polygon(screen, color, points, line_width)
+
+    def update(self, dt):
+        # sub-classes must override
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotation = self.rotate(-1 * dt)
+            
+        if keys[pygame.K_d]:
+            self.rotation = self.rotate(dt)
+    
+    def rotate(self, dt):
+        return ( (PLAYER_TURN_SPEED * dt) + self.rotation )
